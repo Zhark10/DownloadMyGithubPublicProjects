2e0a6f50b1a71fb6cdb2c0448a20f5af61947749
Zhark10 <verygoodman18@yandex.ru>
2022-04-15 22:46:01 +0300

    minor changes

--- ghimp.js
+++ ghimp.js
@@ -1,29 +1,26 @@
 import fetch from "node-fetch";
 import shell from "shelljs";
+
 const FOLDER_NAME = "PUBLIC_PROJECTS"
 let PROGRESS_COUNT = 0
+
 shell.mkdir(FOLDER_NAME)
 shell.cd(`./${FOLDER_NAME}`)
 
-const getRepo = async (repo, _index, array) => {
-  try {
-    shell.exec(`git clone ${repo.html_url}`, () => {
-      PROGRESS_COUNT++
-      console.log('\x1b[32m%s\x1b[0m', `PROJECT ${repo.name} IS DOWNLOADED (PROGRESS: ${PROGRESS_COUNT}/${array.length})`);
-    })
-  } catch (error) {
-    console.log("error by repo", error)
-  }
+const cloneRepo = async (repo, _index, array) => {
+  shell.exec(`git clone ${repo.html_url}`, () => {
+    PROGRESS_COUNT++
+    console.log('\x1b[32m', `REPO "${repo.name}" DOWNLOADED (PROGRESS: ${PROGRESS_COUNT}/${array.length})`);
+  })
 }
 
 const saveReposOnLocal = async () => {
-  let response = await fetch(`https://api.github.com/users/${process.env.USERNAME}/repos?per_page=500`);
-
+  const response = await fetch(`https://api.github.com/users/${process.env.USERNAME}/repos?per_page=1000`);
   if (response.ok) {
-    let repos = await response.json();
-    Promise.all(repos.map(getRepo))
+    const repos = await response.json();
+    Promise.all(repos.map(cloneRepo))
   } else {
-    alert("Ошибка HTTP: " + response.status);
+    console.log('\x1b[31m', "Error HTTP: " + response.status);
   }
 }
 
