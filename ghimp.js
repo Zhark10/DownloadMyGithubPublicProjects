106a237808acf06753bdcd311fd2f2eefc21e526
Zhark10 <verygoodman18@yandex.ru>
2022-04-16 02:04:42 +0300

    minor changes

--- ghimp.js
+++ ghimp.js
@@ -7,18 +7,16 @@ let PROGRESS_COUNT = 0
 shell.mkdir(FOLDER_NAME)
 shell.cd(`./${FOLDER_NAME}`)
 
-const cloneRepo = async (repo, _index, array) => {
-  shell.exec(`git clone ${repo.html_url}`, () => {
-    PROGRESS_COUNT++
-    console.log('\x1b[32m', `REPO "${repo.name}" DOWNLOADED (PROGRESS: ${PROGRESS_COUNT}/${array.length})`);
-  })
-}
+const cloneRepo = (repo, _, array) => shell.exec(`git clone ${repo.html_url}`, () => {
+  PROGRESS_COUNT++
+  console.log('\x1b[32m', `REPO "${repo.name}" DOWNLOADED (PROGRESS: ${PROGRESS_COUNT}/${array.length})`);
+})
 
 const saveReposOnLocal = async () => {
   const response = await fetch(`https://api.github.com/users/${process.env.USERNAME}/repos?per_page=1000`);
   if (response.ok) {
     const repos = await response.json();
-    Promise.all(repos.map(cloneRepo))
+    repos.forEach(cloneRepo)
   } else {
     console.log('\x1b[31m', "Error HTTP: " + response.status);
   }
